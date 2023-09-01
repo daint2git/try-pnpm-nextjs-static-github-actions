ae1a3e235f09b8bbd74fad6c1a438909c3a4cba5
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2023-09-01 18:09:23 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <main className="flex min-h-screen flex-col items-center p-20">
       Home
       <div className="aspect-square relative w-[100px] h-[100px]">
-        <Image src="/vercel.svg" alt="Vercel Logo" priority fill />
+        <Image
+          src={`${router.basePath}/vercel.svg`}
+          alt="Vercel Logo"
+          priority
+          fill
+        />
       </div>
     </main>
   );
