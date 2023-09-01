1cb380807f57fa6056c92c6e9a86b71c78da29db
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2023-09-01 17:48:44 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home1
+    <main className="flex min-h-screen flex-col items-center p-20">
+      Home
+      <div className="aspect-square relative w-[100px] h-[100px]">
+        <Image src="/vercel.svg" alt="Vercel Logo" priority fill />
+      </div>
     </main>
   );
 }
